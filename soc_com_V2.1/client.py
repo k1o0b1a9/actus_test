2f48c882031102d039cc28461ab2a52015e66a3e
K1o0b1a9 <your.email@example.com>
2021-11-24 03:01:54 +0000

    Add IPv6 process and Avoid ZerodivisionError

---
+++ soc_com_V2.1/client.py
@@ -0,0 +1,54 @@
+# Echo client program
+import socket
+import sys
+
+HOST = socket.gethostname()    #ホスト名取得
+PORT = 8080              #ポート番号8080にする
+client = None
+
+response=0 #response初期値
+massage=0 #massage初期値(未定義エラー回避のためexit以外で定義)
+
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        client = socket.socket(af, socktype, proto)
+    except OSError as msg:
+        client = None
+        continue
+    try:
+        client.connect(sa)
+    except OSError as msg:
+        client.close()
+        client = None
+        continue
+    break
+if client is None:
+    print('could not open socket')
+    sys.exit(1)
+    
+while massage != 'exit':
+
+    print("Send Message")
+    
+    try:
+        massage=input()
+    except KeyboardInterrupt as message:
+        massage="exit"
+        client.send(massage.encode('utf-8')) #サーバープログラムにデータ送信
+        client.close()
+        break
+    
+    #入力ない場合は送信しない
+    if not massage:
+        print("文字を入力してください")
+        continue
+    else:
+        client.send(massage.encode('utf-8')) #サーバープログラムにデータ送信
+
+        response = client.recv(4096).decode() #サーバーの処理結果受信(レシーブは適当な2の累乗)
+
+    print(response)
+
+print("計算を終了します")
+client.close()
--- soc_com_V2.1/server.py
+++ soc_com_V2.1/server.py
@@ -1,32 +1,49 @@
-# -*- coding:utf-8 -*-
+# Echo server program
 import socket
 import sys
 
-host = socket.gethostname() #ホスト名取得
-port = 8080 #PORT指定(クライアントと一致していればOK)
+HOST = None               #ホスト名はクライアントに合わせるので不定
+PORT = 8080              #クライアントに合わせて8080
+s = None
 
-#serversock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #オブジェクトの作成
-serversock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #オブジェクトの作成
-serversock.bind((host,port)) #IPとPORTを指定してバインドします
-serversock.listen(1) #接続の待ち受けをします（キューの最大数を指定）
+#プロトコルを不定として環境に合わせてIPv4、IPv6にする
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
+                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+    af, socktype, proto, canonname, sa = res
+    try:
+        s = socket.socket(af, socktype, proto)
+    except OSError as msg:
+        s = None
+        continue
+    try:
+        s.bind(sa)
+        s.listen(1)
+    except OSError as msg:
+        s.close()
+        s = None
+        continue
+    break
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+    
+#本来クライアントプログラムからの操作で終了が正常な使用法だが
+#万一サーバープログラムから終了しようとした場合に備えて実装
+try:
+    conn,addr= s.accept()   #acceptでは受信時に使用するオブジェクトとアドレス情報が生成されるのでアドレス情報用の変数も用意する
+except KeyboardInterrupt as clientsock:
+    print("とじる")
+    sys.exit()
 
 print('Waiting for connections...')
 
 sndmsg=0 #未定義での参照防止のため
 
-#本来クライアントプログラムからの操作で終了が正常な使用法だが
-#万一サーバープログラムから終了しようとした場合に備えて実装
-try:
-    clientsock, client_address = serversock.accept() #接続されればデータを格納
-except KeyboardInterrupt as clientsock:
-    print("とじる")
-    sys.exit()
-
 while True:
     
     #strでクライアントプログラムからの入力を受け取る
-    rcvmsg = clientsock.recv(4096).decode()
-    
+    rcvmsg = conn.recv(4096).decode()
+
     #入力された文字列がexitであった場合、その時点で処理終了
     if rcvmsg == 'exit':
         break
@@ -40,7 +57,7 @@ while True:
             sndmsg = rcvmsg[(rcvmsg.find("=")+1):]
         else:
             #文字列の場合、エラーを直接送りもう一度入力
-            clientsock.sendall("数値を入力してください".encode('utf-8')) 
+            conn.sendall("数値を入力してください".encode('utf-8')) 
             continue
     #足し算
     elif (rcvmsg.find("+"))!=-1:
@@ -49,7 +66,7 @@ while True:
             sndmsg += float(rcvmsg[(rcvmsg.find("+")+1):])
         else:
             #文字列の場合、エラーを直接送りもう一度入力
-            clientsock.sendall("数値を入力してください".encode('utf-8')) 
+            conn.sendall("数値を入力してください".encode('utf-8')) 
             continue
     #引き算
     elif (rcvmsg.find("-"))!=-1:
@@ -58,7 +75,7 @@ while True:
             sndmsg -= float(rcvmsg[(rcvmsg.find("-")+1):])
         else:
             #文字列の場合、エラーを直接送りもう一度入力
-            clientsock.sendall("数値を入力してください".encode('utf-8')) 
+            conn.sendall("数値を入力してください".encode('utf-8')) 
             continue
     #掛け算    
     elif (rcvmsg.find("*"))!=-1:
@@ -67,26 +84,31 @@ while True:
             sndmsg *= float(rcvmsg[(rcvmsg.find("*")+1):])
         else:
             #文字列の場合、エラーを直接送りもう一度入力
-            clientsock.sendall("数値を入力してください".encode('utf-8')) 
+            conn.sendall("数値を入力してください".encode('utf-8')) 
             continue
     #割り算
     elif (rcvmsg.find("/"))!=-1:
         if(str.isdigit(rcvmsg[(rcvmsg.find("/")+1):]))==True:
-            sndmsg=float(sndmsg)
-            sndmsg /= float(rcvmsg[(rcvmsg.find("/")+1):])
+            #0割になる場合は計算行わない
+            if(int(rcvmsg[(rcvmsg.find("/")+1):]))==0:
+                conn.sendall("0割です".encode('utf-8'))
+                continue
+            else:    
+                sndmsg=float(sndmsg)
+                sndmsg /= float(rcvmsg[(rcvmsg.find("/")+1):])
         else:
             #文字列の場合、エラーを直接送りもう一度入力
-            clientsock.sendall("数値を入力してください".encode('utf-8')) 
+            conn.sendall("数値を入力してください".encode('utf-8')) 
             continue
     else:
             #文字列の場合、エラーを直接送りもう一度入力
-            clientsock.sendall("数値を入力してください".encode('utf-8')) 
+            conn.sendall("数値を入力してください".encode('utf-8')) 
             continue
     
-    sndmsg=str(sndmsg)
+    sndmsg=str(sndmsg) #送信時str型のためキャスト
     
-    clientsock.sendall(sndmsg.encode('utf-8')) #メッセージを返します
+    conn.sendall(sndmsg.encode('utf-8')) #メッセージを返します
 
 #クローズ処理 
 print("Now Closing")
-clientsock.close()
+conn.close()
